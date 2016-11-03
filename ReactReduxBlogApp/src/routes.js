6bfb980de419c72efeb859696c4af67de35eb0c9
priyamaurya <priya.maurya1015@gmail.com>
2016-11-03 12:21:10 +0000

    commit changes

---
+++ ReactReduxBlogApp/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route , IndexRoute} from 'react-router';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
+
+import App from './components/app';
+
+export default (
+<Route path="/" component ={App}>
+     <IndexRoute component={PostsIndex}/>
+     <Route path ="/posts_new" component= {PostsNew}/>
+     <Route path="posts/:id" component ={PostsShow}/>
+
+</Route>
+
+);
